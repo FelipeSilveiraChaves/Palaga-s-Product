ce6124e78b9d44804bdae1a7b2a12b2bd8fd1ea5
Felipe Chaves <code.felipechaves@gmail.com>
2025-12-09 16:58:02 -0300

    font apply

--- quatrogavetas-lp1/src/app/layout.tsx
+++ quatrogavetas-lp1/src/app/layout.tsx
@@ -1,18 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from 'next/font/local'
 import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const dinRounded = localFont({
+  src: '../../public/font.woff2',
+})
 
 export const metadata: Metadata = {
   title: "s",
@@ -27,10 +21,10 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-carbon-0`}
+        className={`${dinRounded.className} antialiased min-h-screen bg-carbon-0`}
       >
         <Navbar />
-        {children}
+          {children}
         <Footer />
       </body>
     </html>
