f752f9093c8295f8ecc6585a289476bc589d6b20
Felipe Chaves <code.felipechaves@gmail.com>
2026-02-10 16:53:21 -0300

    starting the first landing page

--- quatrogavetas-lp1/src/app/components/navbar.tsx
+++ quatrogavetas-lp1/src/app/components/navbar.tsx
@@ -3,7 +3,6 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import HamburgerButton from "./hamburgerButton";
-import Logo from "./logo";
 import LogoFake from '../../../assets/logo-exemplo.svg'
   
 
--- quatrogavetas-lp1/src/app/lp-1/page.tsx
+++ quatrogavetas-lp1/src/app/lp-1/page.tsx
@@ -1,12 +1,40 @@
+import Image from "next/image";
 import LocalStorageLpRegister from "../components/localstoragelpregister";
+import { Gabarito } from 'next/font/google'
+
+const gabarito = Gabarito({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  display: 'swap',
+})
 
 export default function LandingPageOne(){
-    return(
+  return(
     <>
       <LocalStorageLpRegister lpId={"lp-1"}/>
-      <div className="h-full text-white p-4 flex flex-col items-center">
-          Começo      
+      <div className="h-full p-4 flex flex-col justify-center items-center w-full">
+        <div className=" w-lg ">
+          <h1
+            className={`${gabarito.className} text-[64px] text-[#202A40] tracking-[-1.28px] leading-16 font-semibold`}
+          >
+            Estão cimplicando aquilo que era simples 
+          </h1>  
+          <p className="text-[20px] text-carbon-600 tracking-normal leading-[30px] pt-[60px] pb-7">
+            In 2015, Ashish, Sukh and I (the Studio March team) were designing apps and websites for some pretty neat companies. We also wanted to build a product of our own that anyone could use and love, but weren’t sure what. Food delivery service? Better credit card? Reliable mobile network?
+          </p>    
+          <p className="text-[20px] text-carbon-600 tracking-normal leading-[30px] pb-[60px]">
+            A hundred different ideas came to us, sitting around a tiny table in our basic tees. Wait... basic tees?! We love those, there aren’t any good options, let’s make one!
+          </p>    
+        </div>
+          <Image
+            src="/images/cadernoMockup.avif"
+            alt="Caderno de planejamento"
+            width={640}
+            height={520}
+            priority
+          />
       </div>
+          
     </>
         
     )
