69fa2d7a7dbe1eb62406a8307930740e554ac1ec
Felipe Chaves <code.felipechaves@gmail.com>
2026-02-10 17:27:02 -0300

    details and avif removed

--- quatrogavetas-lp1/src/app/lp-1/page.tsx
+++ quatrogavetas-lp1/src/app/lp-1/page.tsx
@@ -27,12 +27,24 @@ export default function LandingPageOne(){
           </p>    
         </div>
           <Image
-            src="/images/cadernoMockup.avif"
+            src="/images/cadernomockup2.png"
             alt="Caderno de planejamento"
             width={640}
             height={520}
             priority
           />
+          <div className="w-lg">
+             <h1
+            className={`${gabarito.className} pt-[90px] pb-16 text-[64px] text-[#202A40] tracking-[-1.28px] leading-16 font-semibold`}
+          >
+            Vantagens 
+          </h1>  
+          <p className="text-[20px] text-carbon-600 tracking-normal leading-[30px] pb-7">
+            Ok, maybe it wasn’t quite so quick and clean. But that’s basically how we decided to make the March Tee and became founders overnight.          </p>
+          <p className="text-[20px] text-carbon-600 tracking-normal leading-[30px] pb-7">
+            We were clueless about fashion, manufacturing, even tees. All we knew was good design and telling a compelling story. <span className="bg-[#FFFF0060]">Turns out that’s </span>what was missing <span className="bg-[#FFFF0060]">from the experience</span>, along with an actually well-made tee. So we jumped <span className="bg-[#FFFF0060]">right into making things.</span>          
+            </p>
+          </div>
       </div>
           
     </>
