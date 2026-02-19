369fc11c5c91995f10ad442ad565595bc4b6791a
Felipe Chaves <code.felipechaves@gmail.com>
2026-02-19 12:11:14 -0300

    New NavBar initiate

--- quatrogavetas-lp1/src/app/components/navbar.tsx
+++ quatrogavetas-lp1/src/app/components/navbar.tsx
@@ -1,35 +1,41 @@
 'use client'
 
+import { FaInstagram } from "react-icons/fa6";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import HamburgerButton from "./hamburgerButton";
-import LogoFake from '../../../assets/logo-exemplo.svg'
-  
 
 export default function Navbar(){
-    const [landingOrigin, setLandingOrigin] = useState('lp-1')
-    
+  const [landingOrigin, setLandingOrigin] = useState('lp-1')
 
+  useEffect(() => {
+    const origin = localStorage.getItem('landingOrigin') || "lp-1";
+    setLandingOrigin(origin)
+  }, [landingOrigin])
 
-    useEffect(() => {
-        const origin = localStorage.getItem('landingOrigin') || "lp-1";
-        setLandingOrigin(origin)
-    }, [landingOrigin])
+  return (
+    <div className="
+      w-full 
+      h-16 
+      sticky top-0 left-1/2
+      px-6 pt-4 mb-6 
+      z-50
+      bg-transparent
+    ">
+      <nav className="
+        w-full max-w-[750px] h-14 rounded-full mx-auto flex items-center justify-between pr-[23px] pl-[33px]
+        bg-white/60 backdrop-blur-md
+        border-[#E1E1E1]/70 border 
+        shadow-[0px_2px_10px_rgba(0,0,0,0.03),0px_4px_8px_rgba(0,0,0,0.05)]
+      ">
+        <div>
+          <FaInstagram color="#6f7481" size={26} />
+        </div>
 
-    return (
-        <nav  className="
-            w-full 
-            h-16 
-            sticky top-0 
-            px-6 pt-4 mb-6 
-            z-50
-            bg-transparent
-            shadow-[inset_0_20px_20px_20px_rgba(255,255,255,0.9)]
-        ">
-                <div className="w-full flex justify-between items-start">
-                    <Link href={`/${landingOrigin}`}><LogoFake /></Link>
-                    <HamburgerButton />
-                </div>
-        </nav>
-    )
+        <div className="flex items-center justify-center text-[#6f7481] gap-5 ">
+          <p>Email</p>
+          <FaInstagram color="#6f7481" size={26} />
+        </div>
+      </nav>
+    </div>
+  )
 }
--- quatrogavetas-lp1/src/app/lp-1/components/MainSearchComponent.tsx
+++ quatrogavetas-lp1/src/app/lp-1/components/MainSearchComponent.tsx
@@ -10,16 +10,20 @@ const inter = Inter({
 
 export default function MainSearchComponent() {
 
-
-    const BaseButtonsSearchsStyle =
-  "rounded-full text-[21px] h-[45px] px-[18px] inline-flex items-center justify-center leading-none ml-[75px]" +
-  "bg-white text-[#1F1F1F] border-[1px] border-[#E5E5E5] " +
-  "shadow-[0px_4px_8px_rgba(0,0,0,0.05),0px_0px_0px_1.16px_rgba(0,0,0,0.065)]";
+    const ExamplesVariables = {
+        BaseModel:  "rounded-full text-[21px] h-[45px] px-[18px] inline-flex items-center justify-center leading-none" +
+                    "bg-white text-[#1F1F1F] border-[0,5px] border-[#E5E5E5] " +
+                    "shadow-[0px_4px_8px_rgba(0,0,0,0.05),0px_0px_0px_1.16px_rgba(0,0,0,0.065)]",
+        RedModel:   "rounded-full text-[21px] h-[45px] px-[18px] inline-flex items-center justify-center leading-none " +
+                    "bg-[#FFE4E4] border-[0,5px] border-[#FDEFEE] text-[#1F1F1F] " +
+                    "shadow-[0px_4px_8px_rgba(255,123,123,0.05),0px_0px_0px_1.16px_rgba(226,23,0,0.065)]",
+    }
+   
 
 
     return (
-        <div className="gap-3  flex flex-col items-start">
-            <div className="h-17 min-w-[639px] bg-white border-[1.5px] border-[#DFDFDF] py-2.25 px-1.75 gap-1.75 rounded-[21px] flex items-center justify-between shadow-[0px_2px_10px_rgba(0,0,0,0.03),0px_4px_8px_rgba(0,0,0,0.05)]">
+        <div className="gap-3  flex flex-col items-start ">
+            <div className="h-17 sm:w-[639px] w-full bg-white border border-[#DFDFDF] py-2.25 px-1.75 gap-1.75 rounded-[21px] flex items-center justify-between shadow-[0px_2px_10px_rgba(0,0,0,0.03),0px_4px_8px_rgba(0,0,0,0.05)]">
                 
                 <div className="h-[51px] w-[51px] flex items-center justify-center ">
                     <BiLeftArrowAlt size={30} color="#ABABAB" />
@@ -37,7 +41,7 @@ export default function MainSearchComponent() {
                             />
                         </div>
 
-                        <h3 className={`${inter.className} text-[21px] leading-[30px] flex-1` }>
+                        <h3 className={`${inter.className} text-[21px] leading-[30px] flex-1 ` }>
                             Para ser produtivo, eu preciso de...
                         </h3>
 
@@ -54,14 +58,17 @@ export default function MainSearchComponent() {
 
             </div>
             <div className="flex flex-col gap-3 items-start pl-[78px]">
-                <div className={`${inter.className} ${BaseButtonsSearchsStyle}`}>
+                <div className={`${inter.className} ${ExamplesVariables.BaseModel}`}>
                     Planners
                 </div>
-                <div className={`${inter.className} ${BaseButtonsSearchsStyle}`}>
+                <div className={`${inter.className} ${ExamplesVariables.BaseModel}`}>
                     Hábitos
                 </div>
-                <div className={`${inter.className} ${BaseButtonsSearchsStyle}`}>
+                <div className={`${inter.className} ${ExamplesVariables.BaseModel}`}>
                     Planejamento anual
+                </div>
+                <div className={`${inter.className} ${ExamplesVariables.RedModel}`}>
+                    +147
                 </div>
             </div>
         </div>
