87f9e1d1adb2f25f9165653bf03409913a4f48a4
Felipe Chaves <code.felipechaves@gmail.com>
2026-03-05 15:28:38 -0300

    title small changes

--- quatrogavetas-lp1/src/app/components/badge.tsx
+++ quatrogavetas-lp1/src/app/components/badge.tsx
@@ -8,7 +8,7 @@ export function Badge({ label = "MARCH", className = "" }: BadgeProps) {
     <div
       className={`font-title inline-flex h-12.5 items-center justify-center rounded-full bg-[#CBE9FF] px-3.5 font-extrabold ${className}`}
     >
-      <span className="- text-[40px] leading-12.5 font-normal -tracking-[0.5px] text-[#0D99FF]">
+      <span className="- text-[40px] leading-[49.5] font-normal -tracking-[0.9px] text-[#0D99FF]">
         {label}
       </span>
     </div>
