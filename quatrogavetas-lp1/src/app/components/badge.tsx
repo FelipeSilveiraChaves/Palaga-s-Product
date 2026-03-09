7e628e0835111b780ce7605b53955e2a44d2c845
Felipe Chaves <code.felipechaves@gmail.com>
2026-03-09 12:24:00 -0300

    testes corrigidos de vez

--- quatrogavetas-lp1/src/app/components/badge.tsx
+++ quatrogavetas-lp1/src/app/components/badge.tsx
@@ -6,9 +6,11 @@ type BadgeProps = {
 export function Badge({ label = "MARCH", className = "" }: BadgeProps) {
   return (
     <div
-      className={`font-title inline-flex h-12.75 items-center justify-center rounded-full bg-[#CBE9FF] px-3.5 font-extrabold ${className}`}
+      className={`font-title inline-flex h-12.75 items-center justify-center rounded-full bg-[#CBE9FF] px-3.5 font-extrabold`}
     >
-      <span className="text-[40px] leading-[49.5px] font-normal -tracking-[0.9px] text-[#CBE9FF]">
+      <span
+        className={`text-[40px] leading-[49.5px] font-normal -tracking-[0.9px] ${className} text-[#CBE9FF]`}
+      >
         {label}
       </span>
     </div>
