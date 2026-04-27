323a726154491f9907056a45e4aa3d52347d6e4a
Felipe Silveira Chaves <code.felipechaves@gmail.com>
2026-04-27 16:39:45 -0300

    corner shape in badge

--- quatrogavetas-lp1/src/app/components/badge.tsx
+++ quatrogavetas-lp1/src/app/components/badge.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { Squircle } from "corner-smoothing";
+
 type BadgeProps = {
   label?: string;
   className?: string;
@@ -10,14 +14,16 @@ export function Badge({
   classNameDiv = "",
 }: BadgeProps) {
   return (
-    <div
-      className={`font-title inline-flex h-12.75 items-center justify-center rounded-full bg-[#CBE9FF] px-3.5 align-middle font-extrabold ${classNameDiv}`}
+    <Squircle
+      cornerRadius={17}
+      cornerSmoothing={1}
+      className={`font-title inline-flex items-center justify-center bg-[#CBE9FF] px-3.5 align-middle font-extrabold ${classNameDiv}`}
     >
       <span
-        className={`text-[40px] leading-[49.5px] font-normal -tracking-[0.9px] ${className} text-[#0D99FF]`}
+        className={`text-[40px] leading-[49.5px] font-normal -tracking-[0.9px] text-[#0D99FF] ${className}`}
       >
         {label}
       </span>
-    </div>
+    </Squircle>
   );
 }
